9d8d3040884254d22475c67274dc951f5eb154e4
HatsuneMoe <HatsuneClyne@gmail.com>
2015-12-11 19:20:09 +0800

    Update 0w0.py
    a big mistake [fixed]

--- 0w0.py
+++ 0w0.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 import re
 import urllib.request
 import os
@@ -7,8 +8,11 @@ import time
 class GetHtml2Json():
     PageI = 0
     mName = ""
+    isEnd = False
+    dataJson = []
     def getHtml(self):
-        #print(PageI)
+        print(self.PageI)
+        #print(self.mName)
         NameEncode = urllib.parse.quote_plus(self.mName.encode('GB2312'))
         FullUrl = "http://tieba.baidu.com/f/search/ures?kw=&qw=&rn=10&un=" + \
                  NameEncode + "&only_thread=&sm=0&sd=&ed=&pn=" + str(self.PageI)
@@ -32,29 +36,32 @@ class GetHtml2Json():
         mFileName = self.mName + '.json'
         mFile = open(mFileName, 'a+')
         mList = self.getHtml()
-        print(mList)
-        
-        for s in mList:
-            data={} 
-            data["Title"] = s[0]
-            data["context"] = s[1]
-            data["Tieba"] = s[2]
-            data["Time"] = s[3]
-            jsonStr = json.dumps(data, ensure_ascii = False, indent=2)
-            data.clear()
+        if len(mList) > 1:
+            for s in mList:
+                data={} 
+                data["Title"] = s[0]
+                data["context"] = s[1]
+                data["Tieba"] = s[2]
+                data["Time"] = s[3]
+                self.dataJson.append(data)
+        else:
+            self.isEnd = True
+            jsonStr = json.dumps(self.dataJson, ensure_ascii = False, indent=2)
+            print(jsonStr)
             mFile.write(jsonStr)
             jsonStr = ""
+            del self.dataJson[:]
         mFile.close()
 
 if __name__ == "__main__":
     i = 1
-    PageNum = 50
-    Name = ""
-    while i <= PageNum:
+    Name = ''
+    while True:
         A = GetHtml2Json(i, Name)
         A.getElement()
-        time.sleep(3)
+        print(A.isEnd)
+        if A.isEnd == True:
+            print("end!")
+            break
         i += 1
-
-
-    
+        time.sleep(5)
